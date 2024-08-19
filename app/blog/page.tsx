3d2962b7a7cbb0ac973db7870a1a454a1969fada
arekhalpern <arek.halpern@gmail.com>
2024-08-19 01:24:59 -0400

    coming soon string added to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,7 @@
 export default function page() {
-  return <div>Blog</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold">Coming Soon!</h1>
+    </div>
+  );
 }
